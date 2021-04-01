3fd3db469801f4ef42cbaa7ea72ed4254454b728
sven <sven.haedrich@sevenlab.de>
2021-04-01 16:02:04 +0200

    add print absolute time functionality

--- source/main.py
+++ source/main.py
@@ -1,16 +1,17 @@
 import getopt
 import sys
+from datetime import datetime
 
 import serial
+from termcolor import cprint
 
 import dali
 import dali_error
 import line
 import sender
-from termcolor import cprint
 
 
-def main(serial_port, input_filename, use_color):
+def main(serial_port, input_filename, use_color, absolute_time):
     my_port = serial.Serial(port=serial_port, baudrate=115200)
     my_file = sender.Sender(file_name=input_filename, serial_port=my_port)
     last_timestamp = 0
@@ -25,25 +26,38 @@ def main(serial_port, input_filename, use_color):
             if input_line.type == input_line.COMMAND:
                 dali_frame = dali.Frame(input_line.length, input_line.data, active_device_type)
                 if use_color:
+                    if absolute_time:
+                        cprint('{} : '.format(datetime.now().strftime("%H:%M:%S")), color='yellow', end='')
                     cprint('{:.03f} : {:8.03f} : {} : '.format(input_line.timestamp, delta, dali_frame), color='green',
                            end='')
                     cprint('{}'.format(dali_frame.cmd()), color='white')
                 else:
+                    if absolute_time:
+                        print('{} : '.format(datetime.now().strftime("%H:%M:%S")), end='')
                     print('{:.03f} : {:8.03f} : {} : {}'.format(input_line.timestamp, delta, dali_frame,
                                                                 dali_frame.cmd()))
                 active_device_type = dali_frame.enable
             else:
                 if use_color:
+                    if absolute_time:
+                        cprint('{} : '.format(datetime.now().strftime("%H:%M:%S")), color='yellow', end='')
                     cprint('{:.03f} : {:8.03f} : '.format(input_line.timestamp, delta), color='green', end='')
                     cprint('{}'.format(dali_error.DALIError(input_line.length, input_line.data)), color='red')
                 else:
+                    if absolute_time:
+                        print('{} : '.format(datetime.now().strftime("%H:%M:%S")), end='')
                     print('{:.03f} : {:8.03f} : {}'.format(input_line.timestamp, delta,
                                                            dali_error.DALIError(input_line.length, input_line.data)))
             last_timestamp = input_line.timestamp
     my_port.close()
 
 
+def show_version():
+    print('dali_py version 1.0.2')
+
+
 def show_help():
+    show_version()
     print('usage: -h : help')
     print('       --help')
     print('       -p <port> : use port')
@@ -53,6 +67,13 @@ def show_help():
     print('       -v : show version information')
     print('       --version')
     print('       --nocolor : don\'t use colors')
+    print('       --absolute : add stamp with absolute time')
+    print('output colums:')
+    print('       if enabled: absolute timestamp (from this machine)')
+    print('       relative timestamp in seconds (from interface)')
+    print('       delta time to previous command')
+    print('       hex data received')
+    print('       DALI command translation')
 
 
 # - - - - - - - - - - - - - - - - - - - -
@@ -61,8 +82,9 @@ if __name__ == '__main__':
     filename = ''
     verbose = False
     color = True
+    absolute_time = False
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'hpfvn:', ['help', 'port=', 'file=', 'version', 'nocolor'])
+        opts, args = getopt.getopt(sys.argv[1:], 'hpfv:', ['help', 'port=', 'file=', 'version', 'nocolor', 'absolute'])
     except getopt.GetoptError:
         show_help()
         sys.exit(2)
@@ -75,8 +97,10 @@ if __name__ == '__main__':
         if opt in ('-f', '--file'):
             filename = arg
         if opt in ('-v', '--version'):
-            print('dali_py version 1.0.2')
+            show_version()
         if opt == '--nocolor':
             color = False
+        if opt == '--absolute':
+            absolute_time = True
 
-    main(port, filename, color)
+    main(port, filename, color, absolute_time)
