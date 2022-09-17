574d8031d04af449b825c69d1112c1e041f3e185
SvenHaedrich <sven.haedrich@sevenlab.de>
2022-09-17 19:38:46 +0200

    add tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -5,6 +5,5 @@ source ../env/bin/activate
 echo "--- update requirements"
 pip3 install -r requirements.txt
 echo "--- execute script"
-export PYTHONPATH=$(pwd)
 python3 -m pytest --version
 python3 -m pytest scripts/ $*
