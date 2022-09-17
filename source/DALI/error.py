713f71b4af7ad02e9c4c5f996cb76b0d181d058e
SvenHaedrich <sven.haedrich@sevenlab.de>
2022-09-17 20:27:24 +0200

    add error codes for lunatone usb device

--- source/DALI/error.py
+++ source/DALI/error.py
@@ -1,17 +1,23 @@
 class DALIError:
+    GENERAL = 9
+    FRAME = 10
+    RECOVER = 8
+
 
     def __init__(self, error_code=0, data=0):
         self.data = data
         self.error_code = error_code
-        error_dictionary = {0: 'OK',
-                            1: 'ERROR: RECEIVE START TIMING',
-                            2: 'ERROR: RECEIVE DATA TIMING',
-                            3: 'ERROR: LOOPBACK TIME',
-                            4: 'ERROR: NO CHANGE',
-                            5: 'ERROR: WRONG STATE',
-                            6: 'ERROR: SETTLING TIME VIOLATION',
-                            7: 'ERROR: SYSTEM FAILURE',
-                            8: 'ERROR: RECOVERED FROM FAILURE - BUS OK'
+        error_dictionary = {0: "OK",
+                            1: "ERROR: RECEIVE START TIMING",
+                            2: "ERROR: RECEIVE DATA TIMING",
+                            3: "ERROR: LOOPBACK TIME",
+                            4: "ERROR: NO CHANGE",
+                            5: "ERROR: WRONG STATE",
+                            6: "ERROR: SETTLING TIME VIOLATION",
+                            7: "ERROR: SYSTEM FAILURE",
+                            8: "ERROR: RECOVERED FROM FAILURE - BUS OK",
+                            9: "ERROR: GENERAL",
+                            10: "ERROR: FRAME"
                             }
         if error_code in error_dictionary:
             self.msg = error_dictionary[error_code]
