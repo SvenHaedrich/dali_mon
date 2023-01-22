d59ed1ad176fd42d9a94a1371fcf1136305cc717
SvenHaedrich <sven.haedrich@sevenlab.de>
2023-01-22 21:02:01 +0100

    introduce more error codes

--- source/DALI/error.py
+++ source/DALI/error.py
@@ -1,33 +1,53 @@
 class DALIError:
-    GENERAL = 9
+    OK = 0
+    RECEIVE_START_TIMING = 1
+    RECEIVE_DATA_TIMING = 2
+    LOOPBACK_TIME = 3
+    NO_CHANGE = 4
+    WRONG_STATE = 5
+    SETTLING_TIME_VIOLATION = 6
     FRAME = 10
-    RECOVER = 8
+    SYSTEM_IDLE = 11
+    SYSTEM_FAILURE = 12
+    SYSTEM_RECOVER = 13
+    CAN_NOT_PROCESS = 20
+    BAD_ARGUMENT = 21
+    QUEUE_FULL = 22
+    BAD_COMMAND = 23
 
     def __init__(self, error_code=0, data=0):
         self.data = data
         self.error_code = error_code
         error_dictionary = {
-            0: "OK",
-            1: "ERROR: RECEIVE START TIMING",
-            2: "ERROR: RECEIVE DATA TIMING",
-            3: "ERROR: LOOPBACK TIME",
-            4: "ERROR: NO CHANGE",
-            5: "ERROR: WRONG STATE",
-            6: "ERROR: SETTLING TIME VIOLATION",
-            7: "ERROR: SYSTEM FAILURE",
-            8: "ERROR: RECOVERED FROM FAILURE - BUS OK",
-            9: "ERROR: GENERAL",
-            10: "ERROR: FRAME",
+            self.OK: "OK",
+            self.RECEIVE_START_TIMING: "ERROR: RECEIVE START TIMING",
+            self.RECEIVE_DATA_TIMING: "ERROR: RECEIVE DATA TIMING",
+            self.LOOPBACK_TIME: "ERROR: LOOPBACK TIME",
+            self.NO_CHANGE: "ERROR: NO CHANGE",
+            self.WRONG_STATE: "ERROR: WRONG STATE",
+            self.SETTLING_TIME_VIOLATION: "ERROR: SETTLING TIME VIOLATION",
+            self.FRAME: "ERROR: INVALID FRAME",
+            self.SYSTEM_IDLE: "ERROR: SYSTEM IDLE",
+            self.SYSTEM_FAILURE: "ERROR: SYSTEM FAILURE",
+            self.SYSTEM_RECOVER: "ERROR: SYSTEM RECOVER",
+            self.CAN_NOT_PROCESS: "ERROR: CAN NOT PROCESS",
+            self.BAD_ARGUMENT: "ERROR: BAD ARGUMENT",
+            self.QUEUE_FULL: "ERROR: QUEUE FULL",
+            self.BAD_COMMAND: "ERROR: BAD COMMAND",
         }
         if error_code in error_dictionary:
             self.msg = error_dictionary[error_code]
         else:
-            self.msg = "UNDEFINED ERROR CODE"
+            self.msg = f"UNDEFINED ERROR CODE ({error_code})"
 
     def __str__(self):
         bit = self.data & 0x0FF
         timer_us = (self.data >> 8) & 0x0FFFFF
-        if self.error_code < 7:
+        if (
+            self.error_code == self.RECEIVE_START_TIMING
+            or self.error_code == self.RECEIVE_DATA_TIMING
+            or self.error_code == self.LOOPBACK_TIME
+        ):
             return f"{self.msg} - BIT: {bit} - TIME: {timer_us} USEC"
         else:
             return self.msg
