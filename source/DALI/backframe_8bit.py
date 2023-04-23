a25ce29692b32851e66c2759b1e3ca180ab83400
SvenHaedrich <sven.haedrich@sevenlab.de>
2023-04-23 08:40:18 +0200

    remove default for address field width

--- source/DALI/backframe_8bit.py
+++ source/DALI/backframe_8bit.py
@@ -1,4 +1,4 @@
 class Backframe8Bit:
-    def __init__(self, frame, address_field_width=10):
+    def __init__(self, frame, address_field_width):
         self.address_string = " " * address_field_width
         self.command_string = f"DATA 0x{frame:02X} = {frame:3} = {frame:08b}b"
