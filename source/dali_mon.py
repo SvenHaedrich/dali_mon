d880be1654ffb6a531132e9c6fcb26d0dd295e6e
SvenHaedrich <sven.haedrich@sevenlab.de>
2023-07-31 19:55:31 +0200

    remove support for the --nocolor option. The function is already part of the usual cli toolbox command `ansifilter`.

--- source/dali_mon.py
+++ source/dali_mon.py
@@ -12,43 +12,27 @@ from connection.hid import DaliUsb
 logger = logging.getLogger(__name__)
 
 
-def print_local_time_color(enabled):
+def print_local_time(enabled):
     if enabled:
         time_string = datetime.now().strftime("%H:%M:%S")
         cprint(f"{time_string} | ", color="yellow", end="")
 
 
-def print_local_time(enabled):
-    if enabled:
-        time_string = datetime.now().strftime("%H:%M:%S")
-        print(f"{time_string} | ", end="")
-
-
-def print_command_color(absolute_time, timestamp, delta, dali_command):
-    print_local_time_color(absolute_time)
+def print_command(absolute_time, timestamp, delta, dali_command):
+    print_local_time(absolute_time)
     cprint(
         f"{timestamp:.03f} | {delta:8.03f} | {dali_command} | ", color="green", end=""
     )
     cprint(f"{dali_command.cmd()}", color="white")
 
 
-def print_command(absolute_time, timestamp, delta, dali_command):
+def print_error(absolute_time, timestamp, delta, status):
     print_local_time(absolute_time)
-    print(f"{timestamp:.03f} | {delta:8.03f} | {dali_command} | {dali_command.cmd()}")
-
-
-def print_error_color(absolute_time, timestamp, delta, status):
-    print_local_time_color(absolute_time)
     cprint(f"{timestamp:.03f} | {delta:8.03f} | ", color="green", end="")
     cprint(f"{status.message}", color="red")
 
 
-def print_error(absolute_time, timestamp, delta, status):
-    print_local_time(absolute_time)
-    print(f"{timestamp:.03f} | {delta:8.03f} | {status.message}")
-
-
-def process_line(frame, no_color, absolute_time):
+def process_line(frame, absolute_time):
     if process_line.last_timestamp != 0:
         delta = frame.timestamp - process_line.last_timestamp
     else:
@@ -57,33 +41,27 @@ def process_line(frame, no_color, absolute_time):
         dali_command = DALI.Decode(
             frame.length, frame.data, process_line.active_device_type
         )
-        if no_color:
-            print_command(absolute_time, frame.timestamp, delta, dali_command)
-        else:
-            print_command_color(absolute_time, frame.timestamp, delta, dali_command)
-            process_line.active_device_type = dali_command.get_next_device_type()
+        print_command(absolute_time, frame.timestamp, delta, dali_command)
+        process_line.active_device_type = dali_command.get_next_device_type()
     else:
-        if no_color:
-            print_error(absolute_time, frame.timestamp, delta, frame.status)
-        else:
-            print_error_color(absolute_time, frame.timestamp, delta, frame.status)
+        print_error(absolute_time, frame.timestamp, delta, frame.status)
     process_line.last_timestamp = frame.timestamp
 
 
-def main_usb(no_color, absolute_time):
+def main_usb(absolute_time):
     logger.debug("read from Lunatone usb device")
     dali_connection = DaliUsb()
     dali_connection.start_receive()
     try:
         while True:
             dali_connection.get_next()
-            process_line(dali_connection.rx_frame, no_color, absolute_time)
+            process_line(dali_connection.rx_frame, absolute_time)
     except KeyboardInterrupt:
         print("\rinterrupted")
         dali_connection.close()
 
 
-def main_tty(transparent, no_color, absolute_time):
+def main_tty(transparent, absolute_time):
     logger.debug("read from tty device")
     line = ""
     while True:
@@ -92,20 +70,20 @@ def main_tty(transparent, no_color, absolute_time):
             line = line.strip(" \r\n")
             if len(line) > 0:
                 frame = DaliSerial.parse(line.encode("utf-8"))
-                process_line(frame, no_color, absolute_time)
+                process_line(frame, absolute_time)
             line = ""
 
 
-def main_file(transparent, no_color, absolute_time):
+def main_file(transparent, absolute_time):
     logger.debug("read from file")
     for line in sys.stdin:
         if len(line) > 0:
             frame = DaliSerial.parse(line.encode("utf-8"))
-            process_line(frame, no_color, absolute_time)
+            process_line(frame, absolute_time)
 
 
 @click.command()
-@click.version_option("1.3.0")
+@click.version_option("1.4.0")
 @click.option(
     "-l",
     "--hid",
@@ -113,7 +91,6 @@ def main_file(transparent, no_color, absolute_time):
     is_flag=True,
 )
 @click.option("--debug", help="Enable debug level logging.", is_flag=True)
-@click.option("--nocolor", help="Do not use color coding for output.", is_flag=True)
 @click.option("--echo", help="Echo unprocessed input line to output.", is_flag=True)
 @click.option("--absolute", help="Add absolute local time to output.", is_flag=True)
 def dali_mon(hid, debug, nocolor, echo, absolute):
@@ -128,11 +105,11 @@ def dali_mon(hid, debug, nocolor, echo, absolute):
     process_line.active_device_type = DALI.DeviceType.NONE
     try:
         if hid:
-            main_usb(nocolor, absolute)
+            main_usb(absolute)
         elif sys.stdin.isatty():
-            main_tty(echo, nocolor, absolute)
+            main_tty(echo, absolute)
         else:
-            main_file(echo, nocolor, absolute)
+            main_file(echo, absolute)
     except KeyboardInterrupt:
         print("\rinterrupted")
 
