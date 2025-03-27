d52fefe9f280dc7b79c81a0fab5df373b329eef5
Sven Hädrich <sven.haedrich@sevenlab.de>
2025-03-27 17:48:02 +0100

    Hide the venv directory

--- source/tests/run_hid.sh
+++ source/tests/run_hid.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set +x
-cd ..
-[ -f venv/bin/activate ] || python3 -m venv venv
+[ -f venv/bin/activate ] || python3 -m venv .venv
 echo "--- activate virtual environemnt"
-source ../venv/bin/activate
+source .venv/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install -r ../requirements.txt
 echo "--- execute script"
+cd ..
 coverage run -m pytest tests/hid/ $*
 coverage report
